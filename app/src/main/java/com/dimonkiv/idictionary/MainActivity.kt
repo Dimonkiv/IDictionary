f364dddef6ddd2d74d849bee4a01f60612aaacf6
Dmytro Ivankiv <dmytro.ivankiv@vakoms.com>
2019-06-27 17:07:04 +0300

    Add: base navigation logic

--- app/src/main/java/com/dimonkiv/idictionary/MainActivity.kt
+++ app/src/main/java/com/dimonkiv/idictionary/MainActivity.kt
@@ -2,11 +2,49 @@ package com.dimonkiv.idictionary
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigation: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initBottomNavigationMenu()
+    }
+
+    private fun initBottomNavigationMenu() {
+        bottomNavigation = findViewById(R.id.navigation_menu)
+
+        bottomNavigation.setOnNavigationItemSelectedListener {
+            if (it.itemId == R.id.navigation_dictionary) {
+
+            } else {
+
+            }
+            return@setOnNavigationItemSelectedListener true
+        }
+    }
+
+    fun changeFragment(fragmentData: FragmentData) {
+        val fragmentById = fragmentData.getBundle()
+
+        when (fragmentById) {
+
+        }
+    }
+
+    private fun addFragmentToContainer(fragment: Fragment, bundle: Bundle?) {
+        if (bundle != null) {
+            fragment.arguments = bundle
+        }
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_container, fragment)
+            .addToBackStack(null)
+            .commit()
+
     }
 }
