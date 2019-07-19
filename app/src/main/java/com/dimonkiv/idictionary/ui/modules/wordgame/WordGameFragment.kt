9fdae1f1b22dbe4d6972e62a9deb8ce1be73f0be
DmytroIvankiv <dmytro.ivankiv@vakoms.com>
2019-07-19 16:41:17 +0300

    Integrate mvvm in word game screen

--- app/src/main/java/com/dimonkiv/idictionary/ui/modules/wordgame/WordGameFragment.kt
+++ app/src/main/java/com/dimonkiv/idictionary/ui/modules/wordgame/WordGameFragment.kt
@@ -3,57 +3,98 @@ package com.dimonkiv.idictionary.ui.modules.wordgame
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.*
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.dimonkiv.idictionary.utills.FragmentById
 import com.dimonkiv.idictionary.data.models.FragmentData
 import com.dimonkiv.idictionary.R
 import com.dimonkiv.idictionary.ui.modules.MainActivity
 
-class WordGameFragment : Fragment(), IWordGameContract.Fragment {
+class WordGameFragment : Fragment() {
 
 
     private lateinit var root: View
-    private lateinit var presenter: WordGamePresenter
-    private lateinit var view: WordGameView
+    private lateinit var toolbar: Toolbar
+    private lateinit var originalTV: TextView
+    private lateinit var translatedTV: TextView
+    private lateinit var showTranslateRL: RelativeLayout
 
+    private val mainActivity: MainActivity
+        get() = activity as MainActivity
+
+    private lateinit var viewModel: WordGameViewModel
+
+
+    /*-----------------------------------------------Initialization---------------------------------------------------*/
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         root = inflater.inflate(R.layout.fragment_word_game, container, false)
+        viewModel = ViewModelProviders.of(this).get(WordGameViewModel::class.java)
 
-        initPresenter()
-        initView()
+        initUI()
+        setListeners()
+        subscribeUI()
 
         return root
     }
 
-    private fun initPresenter() {
-        presenter = WordGamePresenter(this)
-    }
-
-    private fun initView() {
-        view = WordGameView(
-            this,
-            presenter,
-            getMainActivity(),
-            context!!,
-            root
-        )
+    private fun initUI() {
+        toolbar = root.findViewById<Toolbar>(R.id.toolbar).apply {
+            mainActivity.setSupportActionBar(this)
+            setHasOptionsMenu(true)
+            setNavigationIcon(R.drawable.ic_arrow_back)
+        }
+        originalTV = root.findViewById(R.id.original_tv)
+        translatedTV = root.findViewById(R.id.translated_tv)
+        showTranslateRL = root.findViewById(R.id.show_translate_rl)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
-        view.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.menu_toolbar_no_search, menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        view.onOptionsItemSelected(item)
+        if (item?.itemId == R.id.settings) {
+            viewModel.onSettingsButtonClick()
+        }
         return super.onOptionsItemSelected(item)
     }
 
-    override fun showPreviousFragment() {
-        getMainActivity().changeFragment(FragmentData(FragmentById.BACK_FRAGMENT))
+    private fun subscribeUI() {
+        viewModel.navigateToPreviousFragment.observe(this, Observer { showPreviousFragment() })
+
+        viewModel.showTranslatedWord.observe(this, Observer { showTranslatedWord() })
     }
 
-    private fun getMainActivity(): MainActivity {
-        return activity as MainActivity
+
+    /*-------------------------------------------Set listeners--------------------------------------------------------*/
+    private fun setListeners() {
+        showTranslateRL.setOnClickListener {
+            viewModel.onShowTranslateButtonClick()
+        }
+
+        toolbar.setNavigationOnClickListener {
+            viewModel.onBackButtonClick()
+        }
     }
 
+
+    /*---------------------------------------------Show initial data--------------------------------------------------*/
+    private fun showTranslatedWord() {
+        showTranslateRL.visibility = View.GONE
+        translatedTV.visibility = View.VISIBLE
+    }
+
+    private fun showTitle() {
+        mainActivity.supportActionBar?.title = "Назва колоди"
+    }
+
+
+    /*--------------------------------------------Other methods-------------------------------------------------------*/
+    private fun showPreviousFragment() {
+        mainActivity.changeFragment(FragmentData(FragmentById.BACK_FRAGMENT))
+    }
 
 }
