d7a4693960a9f892527bf49c6bccfa9bc0581c39
DmytroIvankiv <dmytro.ivankiv@vakoms.com>
2019-07-19 14:08:13 +0300

    Implement mvvm + live data architecture in create card screen

---
+++ app/src/main/java/com/dimonkiv/idictionary/ui/modules/createcard/CreateCardViewModel.kt
@@ -0,0 +1,54 @@
+package com.dimonkiv.idictionary.ui.modules.createcard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.dimonkiv.idictionary.data.FirebaseManager
+import com.dimonkiv.idictionary.data.models.Card
+import com.dimonkiv.idictionary.utills.SingleLiveEvent
+
+class CreateCardViewModel : ViewModel() {
+    private val card = Card()
+
+    private lateinit var _isLoading: MutableLiveData<Boolean>
+    private lateinit var _isShownError: SingleLiveEvent<Any>
+    private val _closeDialogs = SingleLiveEvent<Any>()
+
+
+    /*-----------------------------------------------Get data---------------------------------------------------------*/
+    val closeDialogs: LiveData<Any>
+        get() = _closeDialogs
+
+    val isLoading: LiveData<Boolean>
+        get() = _isLoading
+
+    val isShownError: LiveData<Any>
+        get() = _isShownError
+
+
+    /*-----------------------------------------------Listeners--------------------------------------------------------*/
+    fun onCardTextChanged(card: String) {
+        this.card.title = card
+    }
+
+    fun onAddButtonClick() {
+        _isLoading.value = true
+
+        if (card.title.isEmpty()) {
+            _isShownError.call()
+            _isLoading.value = false
+            return
+        }
+
+        insertCard()
+        _closeDialogs.call()
+    }
+
+
+    /*----------------------------------------------Work with data----------------------------------------------------*/
+    private fun insertCard() {
+        val cardDataSource = FirebaseManager.getInstance().getCardDataSource()
+        cardDataSource.insert(card)
+        _isLoading.value = false
+    }
+}
